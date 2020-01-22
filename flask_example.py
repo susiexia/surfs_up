148591875b22d0cbb1be78a6edee7d176455da5c
Jiqian Xia <susiexia@Jiqians-MBP.lan>
2020-01-22 23:40:33 +0800

    add comment on

--- flask_example.py
+++ flask_example.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+#create a new Flask app instance
+app = Flask(__name__) # magic method in python
 
-@app.route('/')
-def hello_world():
+#create Flask routes: two steps
+@app.route('/') # define the root of route
+def hello_world(): # build a function
     return 'Hello world'
+
+
+# Then use CMD add this file into SLASK_APP envs variable
+# THEN flask run
